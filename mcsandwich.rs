85531a5ca1f780860ebfe960df6f53adb7deb28e
Effectivement <rnssaketo@proton.me>
2026-02-24 13:54:57 -0500

    Add sorting

--- mcsandwich.rs
+++ mcsandwich.rs
@@ -1,70 +1,45 @@
-fn main() {
-    use std::collections::HashMap;
-    let restaurant_name = "McSandwich";
-
-    struct FoodItem {
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct FoodItem {
         name: String,
         price: i32,
         description: String,
     }
+    
+    impl FoodItem {
+        pub fn new(name: &str, price: i32, description: &str) -> Self {
+            FoodItem {
+               name: String::from(name),
+                price,
+                description: String::from(description) 
+            }
+            
+        }
+    }
 
-    let menu = [
-        "hamburger",
-        "fries",
-        "milkshake",
-        "chickenstrips",
-        "chickensandwich",
+
+fn main() {
+    let restaurant_name = "McSandwich";
+    
+    let mut food_items = vec![
+        FoodItem::new("Basic Hamburger", 1, "Your basic hamburger. Always tastes like cardboard!"),
+        FoodItem::new("French Fries", 3, "Delicious fries guaranteed to keep you wanting more."),
+        FoodItem::new("Milkshake", 4, "A delicious and creamy milk-based drink!"),
+        FoodItem::new("Chicken Strips", 3, "Yummy chicken strips. Approved by chicken enjoyers!"),
+        FoodItem::new("Chicken Sandwich", 1, "A basic sandwich with juicy pieces of chicken."),
+        FoodItem::new("Fried Chicken", 6, "Delicious chicken made with breadcrumbs. Our best item!"),
     ];
-    let mut iteminfo = HashMap::new();
-    iteminfo.insert(
-        "hamburger",
-        FoodItem {
-            name: String::from("Basic Hamburger"),
-            price: 1,
-            description: "Your basic hamburger. Always tastes like cardboard!".to_string(),
-        },
-    );
-    iteminfo.insert(
-        "fries",
-        FoodItem {
-            name: String::from("French Fries"),
-            price: 3,
-            description: "Delicious fries guaranteed to keep you wanting more.".to_string(),
-        },
-    );
-    iteminfo.insert(
-        "milkshake",
-        FoodItem {
-            name: String::from("Milkshake"),
-            price: 4,
-            description: "A delicious and creamy milk-based drink!".to_string(),
-        },
-    );
-    iteminfo.insert(
-        "chickenstrips",
-        FoodItem {
-            name: String::from("Chicken Strips"),
-            price: 3,
-            description: "Yummy chicken strips. Approved by chicken enjoyers!".to_string(),
-        },
-    );
-    iteminfo.insert(
-        "chickensandwich",
-        FoodItem {
-            name: String::from("Chicken Sandwich"),
-            price: 1,
-            description: "A basic sandwich with juicy pieces of chicken.".to_string(),
-        },
-    );
+    
+    food_items.sort();
 
     println!("Welcome to {}! Here is our menu.", restaurant_name);
+    println!("It is sorted by price for you poor people.");
     println!("\n---------------------------------------\n");
-    for i in menu {
+    for i in food_items {
         println!(
             "| {} - {}$",
-            iteminfo.get(i).unwrap().name,
-            iteminfo.get(i).unwrap().price
+            i.name,
+            i.price
         );
-        println!("| {}\n", iteminfo.get(i).unwrap().description)
+        println!("| {}\n", i.description)
     }
 }
