2cc03bb36c7d6a99ad17a8ead8732516b4d7df77
Effectivement <rnssaketo@proton.me>
2026-02-24 14:37:07 -0500

    Add CO2 Emission level with enums

--- mcsandwich.rs
+++ mcsandwich.rs
@@ -1,38 +1,48 @@
 #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum Level {
+    High,
+    Mid,
+    Low,
+    Zero
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct FoodItem {
         name: String,
         price: i32,
         description: String,
-    }
+        emissions: Level,
+}
     
-    impl FoodItem {
-        pub fn new(name: &str, price: i32, description: &str) -> Self {
-            FoodItem {
-               name: String::from(name),
-                price,
-                description: String::from(description) 
-            }
-            
+impl FoodItem {
+    pub fn new(name: &str, price: i32, description: &str, emissions: Level) -> Self {
+        FoodItem {
+            name: String::from(name),
+            price,
+            description: String::from(description),
+            emissions
         }
+            
     }
+}
+
 
 
 fn main() {
     let restaurant_name = "McSandwich";
     
     let mut food_items = vec![
-        FoodItem::new("Basic Hamburger", 1, "Your basic hamburger. Always tastes like cardboard!"),
-        FoodItem::new("French Fries", 3, "Delicious fries guaranteed to keep you wanting more."),
-        FoodItem::new("Milkshake", 4, "A delicious and creamy milk-based drink!"),
-        FoodItem::new("Chicken Strips", 3, "Yummy chicken strips. Approved by chicken enjoyers!"),
-        FoodItem::new("Chicken Sandwich", 1, "A basic sandwich with juicy pieces of chicken."),
-        FoodItem::new("Fried Chicken", 6, "Delicious chicken made with breadcrumbs. Our best item!"),
+        FoodItem::new("Basic Hamburger", 1, "Your basic hamburger. Always tastes like cardboard!", Level::High),
+        FoodItem::new("French Fries", 3, "Delicious fries guaranteed to keep you wanting more.", Level::Mid),
+        FoodItem::new("Milkshake", 4, "A delicious and creamy milk-based drink!", Level::Low),
+        FoodItem::new("Chicken Strips", 3, "Yummy chicken strips. Approved by chicken enjoyers!", Level::Mid),
+        FoodItem::new("Chicken Sandwich", 1, "A basic sandwich with juicy pieces of chicken.", Level::Zero),
+        FoodItem::new("Fried Chicken", 6, "Delicious chicken made with breadcrumbs. Our best item!", Level::Mid),
     ];
     
     food_items.sort();
 
     println!("Welcome to {}! Here is our menu.", restaurant_name);
-    println!("It is sorted by price for you poor people.");
     println!("\n---------------------------------------\n");
     for i in food_items {
         println!(
@@ -40,6 +50,16 @@ fn main() {
             i.name,
             i.price
         );
-        println!("| {}\n", i.description)
+        let emissions = match i.emissions {
+            Level::Zero => "None",
+            Level::Low => "Low",
+            Level::Mid => "Medium",
+            Level::High => "High"
+        };
+        
+        
+        println!("| CO2 Emission level: {}", emissions);
+        println!("|");
+        println!("| {}\n", i.description);
     }
 }
